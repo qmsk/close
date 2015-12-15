5289b1a8175d34c2de7976d66226b6d0427c7f9a
Tero Marttila <tero.marttila@aalto.fi>
2015-12-15 19:04:46 +0200

    udp/reciever: log recv

--- udp/receiver.go
+++ udp/receiver.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "log"
     "net"
+    "time"
 )
 
 type ReceiverConfig struct {
@@ -79,27 +80,36 @@ func (self *Receiver) recv() (Packet, error) {
 }
 
 func (self *Receiver) Run() error {
+    var state Payload
+    var startTime time.Time
+    var startSeq uint64
+
     for {
-        var state Payload
-
-        log.Printf("Recv...\n")
-
         if packet, err := self.recv(); err != nil {
             log.Printf("Recv error: %v\n", err)
         } else {
             if packet.Payload.Start != state.Start {
                 state.Start = packet.Payload.Start
                 state.Seq = packet.Payload.Seq
+                startTime = time.Now()
+                startSeq = packet.Payload.Seq
                 self.stats.packetCount = 0
 
                 log.Printf("Recv Start @%v from seq=%v\n", state.Start, state.Seq)
 
             } else if packet.Payload.Seq > state.Seq {
+                state.Seq = packet.Payload.Seq
                 self.stats.packetCount++
 
-                log.Printf("Recv %8d/%8d @ %d\n", self.stats.packetCount, packet.Payload.Seq - state.Seq)
+                duration := time.Since(startTime)
+                offset := state.Seq - startSeq
 
-                state.Seq = packet.Payload.Seq
+                log.Printf("Recv %8d of %8d in %5.2fs = %5.2f/s @ %6.2f%%\n",
+                    self.stats.packetCount, offset, duration.Seconds(),
+                    float64(self.stats.packetCount) / duration.Seconds(),
+                    float64(self.stats.packetCount) / float64(offset) * 100.0,
+                )
+
             } else {
                 self.stats.packetSkip++
 
