547e8cd4c6b0ee7bbf27b916756e4e04ae61e919
Tero Marttila <tero.marttila@aalto.fi>
2015-12-16 15:07:03 +0200

    test udp/send: benchmark UDP and IP

--- udp/send_test.go
+++ udp/send_test.go
@@ -4,7 +4,7 @@ import (
     "testing"
 )
 
-func BenchmarkSend(b *testing.B) {
+func BenchmarkSendUDP(b *testing.B) {
     // setup
     sendConfig := SendConfig{
         DestAddr:   "127.0.0.1",
@@ -18,5 +18,24 @@ func BenchmarkSend(b *testing.B) {
     b.ResetTimer()
 
     // run
-    send.run(0, 0, 100000) // 100k packets at unbound rate
+    send.run(0, 0, 1000000) // 1m packets at unbound rate
+}
+
+func BenchmarkSendIP(b *testing.B) {
+    // setup
+    sendConfig := SendConfig{
+        SourceNet:      "127.0.1.0/24",
+        SourcePortBits: 16,
+        DestAddr:       "127.0.0.1",
+        DestPort:       PORT,
+    }
+    send, err := NewSend(sendConfig)
+    if err != nil {
+        b.Fatal(err)
+    }
+
+    b.ResetTimer()
+
+    // run
+    send.run(0, 0, 1000000) // 1m packets at unbound rate
 }
