8b86e7a0e704d0d1d02de1a3fbcc24baad1500b9
Tero Marttila <tero.marttila@aalto.fi>
2016-01-06 15:49:49 +0200

    control-web: stats UI

--- control-web/static/close/app.js
+++ control-web/static/close/app.js
@@ -1,5 +1,6 @@
 var closeApp = angular.module('closeApp', [
-        'ngRoute'
+        'ngRoute',
+        'angular-flot'
 ]);
 
 closeApp.config(function($routeProvider){
@@ -12,9 +13,41 @@ closeApp.config(function($routeProvider){
             templateUrl: '/close/worker.html',
             controller: 'WorkerCtrl',
         })
+        .when('/stats', {
+            templateUrl: '/close/stats.html',
+            controller: 'StatsCtrl',
+        })
         .otherwise({
             redirectTo: '/workers',
         });
+});
+
+closeApp.controller('StatsCtrl', function($scope, $http) {
+    $http.get('/api/stats/').success(function(data){
+        $scope.statsSeries = data;
+    });
+
+    $scope.chartOptions = {
+        xaxis: { mode: "time" },
+    };
+    $scope.select = function(statsInfo) {
+        $scope.statsInfo = statsInfo;
+
+        $http.get('/api/stats/' + statsInfo.type + '/', {params:{hostname: statsInfo.hostname, instance: statsInfo.instance}}).success(function(data){
+            $scope.statsData = data.map(function(series){
+                var dataPoints = series.points.map(function(point){
+                    var date = new Date(point.time);
+
+                    return [date.getTime(), point.value];
+                });
+
+                return [{
+                    label: series.field,
+                    data: dataPoints
+                }];
+            });
+        });
+    }
 });
 
 closeApp.controller('WorkersCtrl', function($scope, $http) {
