8a3adfeebdce7567dde67b6215c4a078437e5dec
Arseny Kurnikov <akurniko@gmail.com>
2016-08-15 19:22:26 +0300

    shell: add docker clients command

--- shell/docker/clients.go
+++ shell/docker/clients.go
@@ -1,25 +1,62 @@
 package docker
 
 import (
+	"github.com/qmsk/close/control"
+	"fmt"
+	"io"
+	"encoding/json"
 	"log"
+	"os"
 	"github.com/qmsk/close/shell"
+	"github.com/qmsk/close/util"
 )
 
 type ClientsConfig struct {
 }
 
 type ClientsCmd struct {
+	DockerCmd
 	config ClientsConfig
 }
 
 func (config ClientsConfig) Command(options shell.CommonOptions) (shell.Command, error) {
 	clientsCmd := &ClientsCmd{
-		config: config,
+		DockerCmd {
+			url:    options.Url(),
+			user:   options.User(),
+		},
+
+		config,
 	}
 	return clientsCmd, nil
 }
 
+func (cmd ClientsCmd) Url() string {
+	return cmd.url
+}
+
+func (cmd ClientsCmd) User() shell.User {
+	return cmd.user
+}
+
+func (cmd ClientsCmd) Path() string {
+	return "/api/"
+}
+
 func (cmd ClientsCmd) Execute() error {
-	log.Printf("command docker worker, Execute")
-	return nil
+	return shell.MakeHttpRequest(cmd)
+}
+
+func (cmd ClientsCmd) ParseJSON(body io.ReadCloser) error {
+	var res control.APIGet
+	if err := json.NewDecoder(body).Decode(&res); err != nil {
+		return fmt.Errorf("Error decoding controller state: %v", err)
+	} else {
+		outputter := log.New(os.Stdout, "", 0)
+		outputter.Printf("")
+
+		output := util.PrettySprintf("Clients", res.Clients)
+		outputter.Printf(output)
+		return nil
+	}
 }
