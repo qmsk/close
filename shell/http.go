d870647a691681d5615d326de49e12969a02ca8a
Arseny Kurnikov <akurniko@gmail.com>
2016-08-15 18:55:02 +0300

    shell http: more consistent error returns

--- shell/http.go
+++ shell/http.go
@@ -1,6 +1,7 @@
 package shell
 
 import (
+	"fmt"
 	"net/http"
 	"io"
 	"log"
@@ -18,14 +19,12 @@ func MakeHttpRequest(command GenericCommand) error {
 	}
 
 	if req, err := http.NewRequest("GET", url + path, nil); err != nil {
-		log.Printf("http.NewRequest %v %v: %v", url, path, err)
-		return err
+		return fmt.Errorf("http.NewRequest %v %v: %v", url, path, err)
 	} else {
 		req.SetBasicAuth(user.Id, user.Password)
 
 		if resp, err := client.Do(req); err != nil {
-			log.Printf("shell.DoRequest %v: %v", command.Url(), err)
-			return err
+			return fmt.Errorf("http.client.Do %v: %v", url, err)
 		} else {
 			defer resp.Body.Close()
 			log.Printf("Response %v, %v, content length %v\n", resp.Status, resp.Proto, resp.ContentLength)
